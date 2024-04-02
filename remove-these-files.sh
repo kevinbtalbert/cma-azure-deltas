f9e7adac6b905dfc3341e59ee551d5adca59e5bb
Kevin Talbert <ktalbert@cloudera.com>
2024-04-02 18:49:16 -0400

    added scripts

---
+++ remove-these-files.sh
@@ -0,0 +1,15 @@
+rm ./cma-server/config/targets/settings-definition.yml
+rm ./cma-server/config/transitions/pc/settings-definition.yml
+rm ./cma-server/config/transitions/pc/vars-definition.yml
+rm ./cma-pc-ansible/roles/application_migration/copy_local_to_target_hdfs/tasks/main.yml
+rm ./cma-pc-ansible/roles/application_migration/copy_local_to_target_hdfs/defaults/main.yml
+rm ./cma-pc-ansible/roles/replication_manager/create_replication_policy/templates/hbase_replication_policy.json.j2
+rm ./cma-pc-ansible/roles/replication_manager/create_replication_policy/templates/hive_replication_policy.json.j2
+rm ./cma-pc-ansible/roles/replication_manager/create_replication_policy/templates/hdfs_replication_policy.json.j2
+rm ./cma-pc-ansible/roles/replication_manager/classic_clusters/create_aws_credentials/vars/main.yml
+rm ./cma-pc-ansible/roles/replication_manager/classic_clusters/create_aws_credentials/tasks/main.yml
+rm ./cma-pc-ansible/roles/cloudera_manager/setup_external_account/vars/main.yml
+rm ./cma-pc-ansible/playbooks/data_migration/create_hive_policy.yml
+rm ./cma-pc-ansible/playbooks/data_migration/migrate_hdfs_data.yml
+rm ./cma-pc-ansible/playbooks/data_migration/create_hbase_policy.yml
+
